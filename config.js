794da46027cddf695c1da9146cf5a8eb9e1b9735
Wes Pearce <wespearce1@gmail.com>
2021-04-15 20:33:21 -0600

    User can search recipes

---
+++ config.js
@@ -0,0 +1,1 @@
+export const apiKey = 'a7f6b6da74454e9ab836ec70adfe4aa0';
--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import axios from 'axios';
+import { apiKey } from '../../config';
 
 const SearchRecipes = () => {
   const [query, setQuery] = React.useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(query);
+    axios
+      .get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`)
+      .then(({ data }) => console.log(data))
+      .catch((error) => console.log(error));
   };
 
   return (
