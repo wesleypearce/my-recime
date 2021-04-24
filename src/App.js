68458faed256d9f626958b6d84b273c5314f1c26
wesleypearce <wespearce1@gmail.com>
2021-04-24 07:58:16 -0600

    Removed old navbar markup

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ const App = () => {
 
   return (
     <div>
+      <div className="wrapper">
+        <div className="box a">A</div>
+        <div className="box b">B</div>
+        <div className="box c">C</div>
+        <div className="box d">D</div>
+        <div className="box e">E</div>
+      </div>
       <BrowserRouter>
         <Navbar user={user} setUser={setUser} />
         <Switch>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,84 +14,9 @@ const Navbar = ({ user, setUser }) => {
   };
 
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-          />
-        </a>
-
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">Home</a>
-
-          <a className="navbar-item">Documentation</a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item">About</a>
-              <a className="navbar-item">Jobs</a>
-              <a className="navbar-item">Contact</a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item">Report an issue</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <button
-                onClick={(e) => handleClick(e.currentTarget.id)}
-                className="button is-primary"
-                id="signup"
-              >
-                <strong>Sign up</strong>
-              </button>
-              <button
-                onClick={(e) => handleClick(e.currentTarget.id)}
-                className="button is-light"
-                id="login"
-              >
-                Log in
-              </button>
-              <button
-                onClick={(e) => handleClick(e.currentTarget.id)}
-                className="button is-light"
-                id="logout"
-              >
-                Log out
-              </button>
-              <button
-                onClick={(e) => handleClick(e.currentTarget.id)}
-                className="button is-light"
-                id="profile"
-              >
-                Profile
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <h1>Navbar</h1>
+    </div>
   );
 };
 
