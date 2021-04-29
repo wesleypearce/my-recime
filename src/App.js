4549fa751a23e60ac8033fd913ebe5601f7ce29a
Wes Pearce <wespearce1@gmail.com>
2021-04-29 15:31:45 -0600

    Moved landing page from App to Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Profile from './components/Profile';
 import SearchRecipes from './components/SearchRecipes';
 import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 import { auth } from '../firebase';
 
 const App = () => {
@@ -21,43 +22,18 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div className="wrapper">
-          <Navbar user={user} setUser={setUser} />
-
-          <div className="sidebar">
-            <p>
-              My money's in that office, right? If she start giving me some
-              bullshit about it ain't there, and we got to go someplace else and
-              get it, I'm gonna shoot you in the head then and there. Then I'm
-              gonna shoot that bitch in the kneecaps, find out where my goddamn
-              money is. She gonna tell me too. Hey, look at me when I'm talking
-              to you, motherfucker. You listen: we go in there, and that nigga
-              Winston or anybody else is in there, you the first motherfucker to
-              get shot. You understand?
-            </p>
-          </div>
-          <div className="hero">
-            <img
-              src="meal-1200x800.jpg"
-              onError={(error) => console.log(error)}
-            />
-          </div>
-        </div>
         <Switch>
-          <Route path="/login">
-            <Login setUser={setUser} />
-          </Route>
-          <Route path="/signup">
-            <Signup setUser={setUser} />
-          </Route>
-          <Route path="/profile">
-            <Profile user={user} />
-          </Route>
-          <Route path="/search">
-            <SearchRecipes />
+          <Route path="/">
+            <Home />
           </Route>
           <Route path="/dashboard">
-            <Dashboard />
+            <Dashboard user={user} />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signin">
+            <Signup />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="wrapper">
+      <div className="sidebar">
+        <p>
+          My money's in that office, right? If she start giving me some bullshit
+          about it ain't there, and we got to go someplace else and get it, I'm
+          gonna shoot you in the head then and there. Then I'm gonna shoot that
+          bitch in the kneecaps, find out where my goddamn money is. She gonna
+          tell me too. Hey, look at me when I'm talking to you, motherfucker.
+          You listen: we go in there, and that nigga Winston or anybody else is
+          in there, you the first motherfucker to get shot. You understand?
+        </p>
+      </div>
+      <div className="hero">
+        <img src="meal-1200x800.jpg" onError={(error) => console.log(error)} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
