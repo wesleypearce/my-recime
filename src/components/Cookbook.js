c781a8b1ee99db227eaf90fb7c38a3d5bac57fd0
wesleypearce <wespearce1@gmail.com>
2021-05-10 19:16:47 -0600

    Added test recipes

--- src/components/Cookbook.js
+++ src/components/Cookbook.js
@@ -1,7 +1,24 @@
 import * as React from 'react';
+import { recipe } from '../../recipe';
+import { recipe1 } from '../../recipe1';
 
 const Cookbook = () => {
-  return <h1 className="is-size-1">Cookbook</h1>;
+  const [recipes, setRecipes] = React.useState([recipe, recipe1]);
+
+  const renderShoppingList = () => {
+    return recipes.map((recipe) => {
+      return recipe.extendedIngredients.map((ingredient) => {
+        return <li key={ingredient.id}>{ingredient.name}</li>;
+      });
+    });
+  };
+
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <ul>{renderShoppingList()}</ul>
+    </div>
+  );
 };
 
 export default Cookbook;
