4b86d3ebec77920aa804c6206f03dbedb4bccfab
Wes Pearce <wespearce1@gmail.com>
2021-05-11 07:38:41 -0600

    Ingredient match seems to work with test recipes

--- src/components/Cookbook.js
+++ src/components/Cookbook.js
@@ -7,8 +7,36 @@ const Cookbook = () => {
 
   const renderShoppingList = () => {
     return recipes.map((recipe) => {
+      let ingredients = {};
       return recipe.extendedIngredients.map((ingredient) => {
-        return <li key={ingredient.id}>{ingredient.name}</li>;
+        let { name, measures } = ingredient;
+
+        // if this ingredient has not been added to combined ingredients add it to combined ingredients
+        // else check the measurements to see if they match
+        // if they do, add the amounts and return
+        // if they do not, make a string that explains what measurements are needed
+
+        if (!ingredients[name]) {
+          ingredients[name] = measures;
+          ingredients[
+            name
+          ].ingredientString = `${ingredient.amount} ${ingredient.measures.us.unitLong} ${ingredient.name}`;
+        } else if (
+          ingredients[name].measures.us.unitShort ===
+          ingredient.measures.us.unitShort
+        ) {
+          ingredients[name].measures.us.amount =
+            ingredient.measures.us.amount +
+            ingredients[name].measures.us.amount;
+        } else {
+          ingredients[
+            name
+          ].ingredientString = `${ingredients[name].measures.us.amount} ${ingredients[name].measures.us.unitLong} and ${ingredients[name].measures.us.amount} ${ingredient.measures.us.unitLong}`;
+        }
+
+        return (
+          <li key={ingredient.id}>{ingredients[name].ingredientString}</li>
+        );
       });
     });
   };
