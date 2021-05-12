5c10e25c8804a700802f4e87f9fc64bb3b03be33
Wes Pearce <wespearce1@gmail.com>
2021-05-11 18:31:38 -0600

    Commit before changing Cookbook

--- src/components/Cookbook.js
+++ src/components/Cookbook.js
@@ -5,41 +5,55 @@ import { recipe1 } from '../../recipe1';
 const Cookbook = () => {
   const [recipes, setRecipes] = React.useState([recipe, recipe1]);
 
+  const sortIngredientsByAisle = (ingredients) => {
+    // the below method is giving me an array with 'aisle' prop inside an object one object down
+    const newIngredients = Object.values(ingredients).sort((a, b) =>
+      a.aisle > b.aisle ? 1 : b.aisle > a.aisle ? -1 : 0,
+    );
+    return newIngredients;
+  };
+
   const renderShoppingList = () => {
-    return recipes.map((recipe) => {
-      let ingredients = {};
+    let ingredients = {};
+    let sortedIngredients = [];
+
+    recipes.map((recipe) => {
       return recipe.extendedIngredients.map((ingredient) => {
-        let { name } = ingredient;
-        let { amount, unitLong } = ingredient.measures.us;
+        let { name, aisle, amount, unit, id } = ingredient;
 
         // if this ingredient has not been added to combined ingredients add it to combined ingredients
         // else check the measurements to see if they match
-        // if they do, add the amounts and return
+        // if they do, add the amounts
         // if they do not, make a string that explains what measurements are needed
 
         if (!ingredients[name]) {
-          ingredients[name] = ingredient.measures;
-          ingredients[name].ingredientString = `${amount} ${unitLong} ${name}`;
-        } else if (ingredients[name].measures.us.unitLong === unitLong) {
-          ingredients[name].measures.us.amount =
-            amount + ingredients[name].measures.us.amount;
+          ingredients[name] = { aisle, id };
+          ingredients[name].ingredientString = `${amount} ${unit} ${name}`;
+        } else if (ingredients[name].unit === unit) {
+          ingredients[name].amount = amount + ingredients[name].amount;
         } else {
           ingredients[
             name
-          ].ingredientString = `${ingredients[name].measures.us.amount} ${ingredients[name].measures.us.unitLong} and ${ingredients[name].measures.us.amount} ${ingredient.measures.us.unitLong}`;
+          ].ingredientString = `${ingredients[name].amount} ${ingredients[name].unit} and ${ingredients[name].amount} ${ingredient.unit}`;
         }
-
-        return (
-          <li key={ingredient.id}>{ingredients[name].ingredientString}</li>
-        );
+        sortedIngredients = sortIngredientsByAisle(ingredients);
       });
+    });
+    console.log(sortedIngredients);
+    return sortedIngredients.map((ingredient) => {
+      return (
+        <div key={ingredient.id}>
+          <div className="ingredient">{ingredient.ingredientString}</div>
+          <div className="aisle">{ingredient.aisle}</div>
+        </div>
+      );
     });
   };
 
   return (
     <div>
       <h1>Shopping List</h1>
-      <ul>{renderShoppingList()}</ul>
+      {renderShoppingList()}
     </div>
   );
 };
